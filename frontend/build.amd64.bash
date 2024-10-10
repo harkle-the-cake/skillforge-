41b4e8ce2b7ada5c868205807b56174e8217990e
Pat <harkle@gmx.net>
2024-10-10 18:19:52 +0200

    docker build scripts added

---
+++ frontend/build.amd64.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+ARTIFACT=skillforge-spa
+VERSION=$(jq -r '.version' package.json)
+REPO_ADDRESS=192.168.180.223:5000
+ARCH=amd64
+
+echo "creating docker with $VERSION..."
+
+# Build the Docker image
+if docker build -t $ARTIFACT.$ARCH:$VERSION ./; then
+  echo "Docker build successful."
+else
+  echo "Docker build failed."
+  exit 1
+fi
+
+# Tag the Docker image
+if docker tag $ARTIFACT.$ARCH:$VERSION $REPO_ADDRESS/$ARTIFACT.$ARCH:$VERSION; then
+  echo "Docker tag with $VERSION successful."
+else
+  echo "Docker tag failed."
+  exit 1
+fi
+
+# Push the Docker image
+if docker push $REPO_ADDRESS/$ARTIFACT.$ARCH:$VERSION; then
+  echo "Docker push successful."
+else
+  echo "Docker push failed."
+  exit 1
+fi
